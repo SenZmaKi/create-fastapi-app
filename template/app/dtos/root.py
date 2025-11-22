601de61bb891e69fbb09bf5384d6b7f060446d39
Sen ZmaKi <phidelalvin@gmail.com>
2025-11-22 14:17:31 +0300

    Update root response schema

--- template/app/dtos/root.py
+++ template/app/dtos/root.py
@@ -1,3 +1,5 @@
+from typing import Self
+from fastapi import Request
 from app.utils.settings import settings
 from pydantic import BaseModel
 
@@ -6,10 +8,10 @@ class RootResponse(BaseModel):
     name: str = settings.app_name
     description: str = settings.app_description
     version: str = settings.app_version
-    docs: str = "/docs"
-    redoc: str = "/redoc"
+    docs: str
+    redoc: str
 
     @classmethod
-    def from_request(cls, request):
+    def from_request(cls, request: Request) -> Self:
         base = str(request.base_url).rstrip("/")
         return cls(docs=f"{base}/docs", redoc=f"{base}/redoc")
